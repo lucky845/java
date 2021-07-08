fe6e874fb36616cef76c295c0e2af6ee778b83ea
lucky845 <84754814+lucky845@users.noreply.github.com>
2021-07-08 21:46:56 +0800

    习题

--- day07/src/练习一/CollectionTest.java
+++ day07/src/练习一/CollectionTest.java
@@ -2,8 +2,7 @@ package 练习一;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 
 /**
  * @author lichuang
@@ -25,4 +24,81 @@ public class CollectionTest {
 
     }
 
+    // 在List内去除重复数字值，要求尽量简单
+    public static List duplicateList(List list) {
+        HashSet set = new HashSet();
+        set.addAll(list);
+        return new ArrayList(set);
+    }
+
+    @Test
+    public  void test2() {
+        List list = new ArrayList();
+        list.add(new Integer(1));
+        list.add(new Integer(2));
+        list.add(new Integer(2));
+        list.add(new Integer(4));
+        list.add(new Integer(4));
+        List list2 = duplicateList(list);
+        for (Object integer : list2) {
+            System.out.println(integer);
+        }
+    }
+
+    //
+    @Test
+    public void test3(){
+        HashSet set = new HashSet();
+        Person p1 = new Person(1001,"AA");
+        Person p2 = new Person(1002,"BB");
+
+        set.add(p1);
+        set.add(p2);
+        System.out.println(set);
+
+        p1.name = "CC";
+        set.remove(p1);
+        System.out.println(set);
+
+        set.add(new Person(1001,"CC"));
+        System.out.println(set);
+
+        set.add(new Person(1001,"AA"));
+        System.out.println(set);
+    }
+
+    private class Person { // 内部类
+        private int id;
+        private String name;
+
+        public Person() {
+        }
+
+        public Person(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return id == person.id && Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name);
+        }
+
+    }
 }
