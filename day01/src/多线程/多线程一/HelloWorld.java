55794d35a210ec88fedb1b6747b518c1b6ab2591
lucky845 <84754814+lucky845@users.noreply.github.com>
2021-07-18 16:20:47 +0800

    练习

---
+++ day01/src/多线程/多线程一/HelloWorld.java
@@ -0,0 +1,18 @@
+package 多线程.多线程一;
+
+    public class HelloWorld {
+
+        static int num = 10;
+        public static final int NUMBER = 1;
+
+        public static void main(String[] args) {
+            System.out.println(123);
+            System.out.println(123);
+
+            System.out.println("HelloWorld.main");
+            System.out.println("args = [" + args + "]");
+
+            System.out.println("num = " + num);
+        }
+
+}
